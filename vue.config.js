4c50b1c00c1774623a1ce743cf7045d30bb8abda
the8thbit <the8thbit@gmail.com>
2020-09-27 04:50:11 +0000

    add ssl cert config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+const fs = require('fs')
 
+module.exports = {
+  devServer: {
+    disableHostCheck: true,
+    https: {
+      key: fs.readFileSync(process.env.SSL_KEY),
+      cert: fs.readFileSync(process.env.SSL_CERT)
+    }
+  }
 };
